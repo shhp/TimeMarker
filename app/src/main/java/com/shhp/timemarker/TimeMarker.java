e887f7d15ec9b74995f7e88d1b497f0a79f8efaf
shhp <herculeshhp@gmail.com>
2016-10-22 13:36:28 +0800

    marks can be grouped

--- app/src/main/java/com/shhp/timemarker/TimeMarker.java
+++ app/src/main/java/com/shhp/timemarker/TimeMarker.java
@@ -18,10 +18,14 @@ package com.shhp.timemarker;
 
 import com.shhp.timemarker.log.LogUtil;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
@@ -47,76 +51,164 @@ import java.util.TreeMap;
  * }
  * </pre>
  *
+ * You can group marks by calling {@link TimeMarker#beginGroup()} and {@link TimeMarker#endGroup()}.
+ * Time distribution will be calculated separately among groups.
+ * <br/>
+ *
  * You can call {@link TimeMarker#mark(String)} wherever in the project.
- * When you want to see the time distribution, just call {@link TimeMarker#report(LogUtil)}.
+ * When you want to see the time distribution, just call {@link TimeMarker#report(LogUtil)} or {@link TimeMarker#reportSequentially(LogUtil)}.
  */
 public class TimeMarker {
 
     public static final String TAG = "TimeMarker";
 
-    private static LinkedHashMap<String, Long> sTimestampMap = new LinkedHashMap<String, Long>();
-    private static Map<String, Integer> sKeyCount = new HashMap<String, Integer>();
+    private static LinkedHashMap<String, Long> sCurrentTimestampMap;
+    private static List<LinkedHashMap<String, Long>> sTimestampMapList = new ArrayList<>();
+    private static Map<String, Integer> sCurrentKeyCount;
+    private static ArrayDeque<LinkedHashMap<String, Long>> sTimestampMapStack = new ArrayDeque<>();
+    private static ArrayDeque<Map<String, Integer>> sKeyCountStack = new ArrayDeque<>();
 
     /**
      * Make a mark.
      * @param key A unique key to identify the mark.
      */
     public synchronized static void mark(String key) {
+        if (sCurrentTimestampMap == null) {
+            sCurrentTimestampMap = new LinkedHashMap<>();
+        }
+        if (sCurrentKeyCount == null) {
+            sCurrentKeyCount = new HashMap<>();
+        }
         long time = System.currentTimeMillis();
-        if (sKeyCount.containsKey(key)) {
-            int previousCount = sKeyCount.get(key);
+        if (sCurrentKeyCount.containsKey(key)) {
+            int previousCount = sCurrentKeyCount.get(key);
             previousCount++;
-            sKeyCount.put(key, previousCount);
+            sCurrentKeyCount.put(key, previousCount);
             key += "("+previousCount+")";
         } else {
-            sKeyCount.put(key, 0);
+            sCurrentKeyCount.put(key, 0);
         }
-        sTimestampMap.put(key, time);
+        sCurrentTimestampMap.put(key, time);
     }
 
     /**
-     * Log the time distribution among marks in descending order.
+     * Begin a group.
+     */
+    public synchronized static void beginGroup() {
+        if (sCurrentKeyCount != null) {
+            sKeyCountStack.push(sCurrentKeyCount);
+        }
+        if (sCurrentTimestampMap != null) {
+            sTimestampMapStack.push(sCurrentTimestampMap);
+        }
+        sCurrentTimestampMap = new LinkedHashMap<>();
+        sCurrentKeyCount = new HashMap<>();
+    }
+
+    /**
+     * End a group.
+     */
+    public synchronized static void endGroup() {
+        sTimestampMapList.add(sCurrentTimestampMap);
+        sCurrentTimestampMap = sTimestampMapStack.isEmpty() ? null : sTimestampMapStack.pop();
+        sCurrentKeyCount = sKeyCountStack.isEmpty() ? null : sKeyCountStack.pop();
+    }
+
+    /**
+     * Log the time distribution among marks by group in descending order.
      * @param logUtil
      */
     public synchronized static void report(LogUtil logUtil) {
-        if (sTimestampMap.size() <= 1) {
+        if (sCurrentTimestampMap != null) {
+            sTimestampMapList.add(sCurrentTimestampMap);
+        }
+        if (sTimestampMapList.size() < 1) {
             logUtil.log("Nothing to report!");
         } else {
-            Iterator<String> iterator = sTimestampMap.keySet().iterator();
-            Map<Double, String> orderedTimeCost = new TreeMap<Double, String>(new Comparator<Double>() {
-                @Override
-                public int compare(Double aDouble, Double t1) {
-                    double v1 = aDouble;
-                    double v2 = t1;
-                    return v1 < v2 ? 1
-                            : v1 == v2 ? 0 : -1;
+            int groupCount = 1;
+            for (LinkedHashMap<String, Long> timestampMap : sTimestampMapList) {
+                if (timestampMap.size() > 1) {
+                    logUtil.log(String.format(Locale.US, "/************** group %d ****************/", groupCount++));
+                    Iterator<String> iterator = timestampMap.keySet().iterator();
+                    Map<Double, String> orderedTimeCost = new TreeMap<Double, String>(new Comparator<Double>() {
+                        @Override
+                        public int compare(Double aDouble, Double t1) {
+                            double v1 = aDouble;
+                            double v2 = t1;
+                            return v1 < v2 ? 1
+                                    : v1 == v2 ? 0 : -1;
+                        }
+                    });
+                    String from = iterator.next();
+                    String to = "";
+                    long totalTime = 0;
+                    Random random = new Random();
+                    do {
+                        to = iterator.next();
+                        String key = from + " ---> " + to;
+                        long cost = timestampMap.get(to) - timestampMap.get(from);
+                        orderedTimeCost.put(cost+random.nextDouble(), key);
+                        totalTime += cost;
+                        from = to;
+                    } while (iterator.hasNext());
+                    printResult(orderedTimeCost, (int)totalTime, logUtil);
                 }
-            });
-            String from = iterator.next();
-            String to = "";
-            long totalTime = 0;
-            Random random = new Random();
-            do {
-                to = iterator.next();
-                String key = from + " ---> " + to;
-                long cost = sTimestampMap.get(to) - sTimestampMap.get(from);
-                orderedTimeCost.put(cost+random.nextDouble(), key);
-                totalTime += cost;
-                from = to;
-            } while (iterator.hasNext());
-
-            printResult(orderedTimeCost, totalTime, logUtil);
+            }
         }
-        sTimestampMap.clear();
-        sKeyCount.clear();
+        sTimestampMapList.clear();
+        sTimestampMapStack.clear();
+        sKeyCountStack.clear();
+        sCurrentKeyCount = null;
+        sCurrentTimestampMap = null;
     }
 
-    private static void printResult(Map<Double, String> map, long totalTime, LogUtil logUtil) {
+    /**
+     * Log the time distribution among marks by group sequentially.
+     * @param logUtil
+     */
+    public synchronized static void reportSequentially(LogUtil logUtil) {
+        if (sCurrentTimestampMap != null) {
+            sTimestampMapList.add(sCurrentTimestampMap);
+        }
+        if (sTimestampMapList.size() < 1) {
+            logUtil.log("Nothing to report!");
+        } else {
+            int groupCount = 1;
+            for (LinkedHashMap<String, Long> timestampMap : sTimestampMapList) {
+                if (timestampMap.size() > 1) {
+                    logUtil.log(String.format(Locale.US, "/************** group %d ****************/", groupCount++));
+                    Iterator<String> iterator = timestampMap.keySet().iterator();
+                    String from = iterator.next();
+                    String to = "";
+                    long totalTime = 0;
+                    Random random = new Random();
+                    Map<Double, String> orderedTimeCost = new LinkedHashMap<>();
+                    do {
+                        to = iterator.next();
+                        String key = from + " ---> " + to;
+                        long cost = timestampMap.get(to) - timestampMap.get(from);
+                        orderedTimeCost.put(cost+random.nextDouble(), key);
+                        totalTime += cost;
+                        from = to;
+                    } while (iterator.hasNext());
+
+                    printResult(orderedTimeCost, (int)totalTime, logUtil);
+                }
+            }
+        }
+        sTimestampMapList.clear();
+        sTimestampMapStack.clear();
+        sKeyCountStack.clear();
+        sCurrentKeyCount = null;
+        sCurrentTimestampMap = null;
+    }
+
+    private static void printResult(Map<Double, String> map, int totalTime, LogUtil logUtil) {
         logUtil.log("=================================================");
 
         for (Map.Entry<Double, String> entry : map.entrySet()) {
             double timeCost = entry.getKey();
-            logUtil.log(String.format("║ %s  cost: %.2f  percentage: %.2f%%", entry.getValue(), timeCost, (timeCost * 100f / totalTime)));
+            logUtil.log(String.format("║ %s  cost: %d  percentage: %.2f%%", entry.getValue(), (int)timeCost, ((int)timeCost * 100f / totalTime)));
         }
 
         logUtil.log("=================================================");
